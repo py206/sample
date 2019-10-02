db69df7cf0c308116dded17ee92ffbe1e2ac903b
py206 <penny880206@gmail.com>
2019-10-02 23:02:27 +0800

    first commit

---
+++ sample/sample3.java
@@ -0,0 +1,10 @@
+package project1;
+
+public class sample3 {
+	public static void main(String[] args)
+	{
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava!");
+		System.out.println(123);
+	}
+}
